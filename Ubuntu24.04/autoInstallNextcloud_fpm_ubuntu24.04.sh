f9bebb8c467ceb76b1b993e483fd14fd407e1e6e
Joe <45982832+JoeMrCoffee@users.noreply.github.com>
2025-06-13 19:49:52 +0800

    Add files via upload

---
+++ Ubuntu24.04/autoInstallNextcloud_fpm_ubuntu24.04.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+#this script must be run as root (sudo)
+#Install the latest version of Nextcloud with Maria DB and Redis on Debian / Ubuntu systems
+#UBUNTU 24.04
+echo "This script will help install the necessary packages and files for a minimal Nextcloud install on Debian 12 systems. Includes Apache, Redis, MariaDB, and the latest Nextcloud version. NOTE: if using Ubuntu LTS please exit the script and change the PHP version to 8.1 on line 6 of the script."
+sudo apt update
+sudo apt install -y apache2
+sudo apt install -y memcached
+sudo apt install -y php8.3 
+sudo apt install -y php-mysql php-curl 
+sudo apt install -y php-apcu php-bcmath php-dom php-gd php-gmp 
+sudo apt install -y php-memcached php8.3-fpm
+sudo apt install -y php-redis
+sudo apt install -y php-ldap
+sudo apt install -y php-zip php-mbstring
+sudo apt install -y php-imagick php-intl php-bz2
+sudo apt install -y libmagickcore-6.q16-6-extra
+sudo apt install -y ffmpeg
+sudo apt install -y nfs-common
+sudo apt install -y unzip
+# Update PHP default values
+# Noted that in Ubuntu 24.04 the default php.ini file appears blank
+sudo sed -i 's/memory_limit = 128M/memory_limit = 4096M/' /etc/php/8.3/fpm/php.ini
+sudo sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 2048M/' /etc/php/8.3/fpm/php.ini
+sudo sed -i 's/post_max_size = 8M/post_max_size = 2048M/' /etc/php/8.3/fpm/php.ini
+sudo sed -i 's/output_buffering = 4096/output_buffering = off/' /etc/php/8.3/fpm/php.ini
+sudo sed -i 's/;opcache.interned_strings_buffer=8/opcache.interned_strings_buffer=32/' /etc/php/8.3/fpm/php.ini
+sudo sed -i 's/expose_php = on/expose_php = off/' /etc/php/8.3/fpm/php.ini
+echo "apc.enable_cli = 1" | sudo tee --append /etc/php/8.3/fpm/php.ini
+echo "apc.enable_cli = 1" | sudo tee --append /etc/php/8.3/mods-available/apcu.ini
+echo "ServerTokens Prod" | sudo tee --append /etc/apache2/apache2.conf
+echo "ServerSignature Off" | sudo tee --append /etc/apache2/apache2.conf
+
+# Sizing the workers for php-fpm
+sudo sed -i 's/pm.max_children = 5/pm.max_children = 500/' /etc/php/8.3/fpm/pool.d/www.conf
+sudo sed -i 's/pm.start_servers = 2/pm.start_servers = 10/' /etc/php/8.3/fpm/pool.d/www.conf
+sudo sed -i 's/pm.min_spare_servers = 1/pm.min_spare_servers = 10/' /etc/php/8.3/fpm/pool.d/www.conf
+sudo sed -i 's/pm.max_spare_servers = 3/pm.max_spare_servers = 30/' /etc/php/8.3/fpm/pool.d/www.conf
+
+#add Apache mods
+sudo a2enmod rewrite
+sudo a2enmod headers
+sudo a2enmod env
+sudo a2enmod dir
+sudo a2enmod mime
+sudo a2enmod proxy_fcgi setenvif
+sudo a2enconf php8.3-fpm
+
+#restart the services
+sudo systemctl restart php8.3-fpm
+sudo systemctl restart apache2
+
+
+#Install and configure MariaDB - 
+#will require some action by the admin during the secure
+sudo apt install -y mariadb-server
+sudo mysql_secure_installation
+sudo systemctl restart mariadb
+echo "Create a user for nextcloud;the user is NCadmin, default password is my5QLnextcloud, and database is ncDB."
+sudo mariadb -e "CREATE USER 'NCadmin'@'localhost' IDENTIFIED BY 'my5QLnextcloud';"
+sudo mariadb -e "CREATE DATABASE IF NOT EXISTS ncDB CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
+sudo mariadb -e "GRANT ALL PRIVILEGES ON ncDB.* TO 'NCadmin'@'localhost';"
+sudo mariadb -e "FLUSH PRIVILEGES;"
+echo "DB criteria entered."
+
+sudo systemctl restart mariadb
+
+wget https://download.nextcloud.com/server/releases/latest.zip
+unzip latest.zip
+sudo systemctl stop apache2
+sudo mv /var/www/html/index.html /var/www/html/debian.html
+sudo cp -t /var/www/html -R nextcloud/* nextcloud/.htaccess nextcloud/.user.ini nextcloud/.reuse
+sudo mkdir /var/www/html/custom_apps
+sudo chown -R www-data:www-data /var/www/html/
+sudo systemctl start apache2
+
+echo "Please navigate in a browser from a device on the same network to the IP address or domain name of the system, normally number 2 in the below output."
+ip a
+echo "Fill in the admin user information, and connect the database to MySQL type with localhost and the password set for user nextcloud.
+Once complete please also run the post install script - postInstallNextcloud - to properly configure Redis and memcache for better performance.
+Reminder: Important account info:
+- Your DB user is NCadmin
+- Your DB password: my5QLnextcloud
+- You DB for Nextcloud: ncDB"
---
+++ Ubuntu24.04/postInstallNextcloud_fpm.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#This is the post install script to complete the setup of Redis and memcache 
+echo "Nextcloud base install, post install script."
+sudo systemctl stop apache2
+sudo cp /var/www/html/config/config.php /var/www/html/config/config.php.bak
+sudo cp /var/www/html/config/config.php ./
+sudo cp /etc/apache2/sites-available/000-default.conf /etc/apache2/sites-available/000-default.conf.bak
+sudo cp /etc/apache2/sites-available/000-default.conf ./
+#Run python to update the configs - if split out across different machines, be sure to update the Redis target in the conif.php modification in the python variable.
+sudo python3 ./configModify.py
+sudo cp newConfig.php /var/www/html/config/config.php
+sudo cp ./001-default.conf /etc/apache2/sites-available/000-default.conf
+
+#Install Redis cache and enable caching
+sudo apt install -y redis
+sudo systemctl enable memcached
+sudo systemctl start memcached
+redischk=$(redis-cli ping)
+if [[ $redischk == "PONG" ]]; then
+	echo "PING PONG Success! Redis Online";
+else 
+	echo "Redis check failed, could be a timing issue. Please check manually: redis-cli ping";
+fi
+sudo sed -i 's%session.save_handler = files%session.save_handler = redis%' /etc/php/8.3/fpm/php.ini
+sudo sed -i 's%;session.save_path = "/var/lib/php/sessions"%session.save_path = "tcp://localhost:6379"%' /etc/php/8.3/fpm/php.ini
+
+sudo systemctl restart php8.3-fpm
+sudo systemctl start apache2
+sudo -u www-data php /var/www/html/occ maintenance:repair --include-expensive
+echo "************ Setup complete. Please re-login and enjoy Nextcloud! 
+If you have any questions please reach out to your Nextcloud contact(s) or reach us at https://nextcloud.com/contact/"
